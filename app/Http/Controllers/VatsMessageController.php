68bbf64b9fa0c25fcda8bc58607dbec5867f329a
asmi046 <asmi046@gmail.com>
2024-02-10 18:00:48 +0300

    Ошибка

--- app/Http/Controllers/VatsMessageController.php
+++ app/Http/Controllers/VatsMessageController.php
@@ -10,9 +10,10 @@ class VatsMessageController extends Controller
 {
     public function index(Request $request) {
         if (!$request->has('crm_token')) abort('419');
+        $crm_token = $request->input('crm_token');
         if ($crm_token == "112233") abort('419');
 
-        $crm_token = $request->input('crm_token');
+
 
         VatsMessage::create(
            [
