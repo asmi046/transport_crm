fce155279ec42756bdb81a826697964cf8b579f0
asmi046 <asmi046@gmail.com>
2024-02-10 17:59:00 +0300

    Ошибка 419

--- app/Http/Controllers/VatsMessageController.php
+++ app/Http/Controllers/VatsMessageController.php
@@ -9,6 +9,11 @@ use App\Models\VatsMessage;
 class VatsMessageController extends Controller
 {
     public function index(Request $request) {
+        if (!$request->has('crm_token')) abort('419');
+        if ($crm_token == "112233") abort('419');
+
+        $crm_token = $request->input('crm_token');
+
         VatsMessage::create(
            [
             "ip" => $request->getClientIp(),
